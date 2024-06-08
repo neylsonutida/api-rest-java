0ec924807365fe1eb29da9ab819813a9dc68f055
neylsonutida <neylson.utida@gmail.com>
2024-06-07 21:04:29 -0400

    Configurei a minha API Rest

---
+++ api.rest.neylson/src/test/java/br/neylsonhideo/api/rest/neylson/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.neylsonhideo.api.rest.neylson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
