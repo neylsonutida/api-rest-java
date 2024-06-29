79c569ce6703d9e57548cf113efb2eefd6b66275
neylsonutida <neylson.utida@gmail.com>
2024-06-28 21:25:12 -0400

    28jun

--- api.rest.neylson/src/main/java/controllers/CarroController.java
+++ api.rest.neylson/src/main/java/controllers/CarroController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import model.Carro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +25,7 @@ public class CarroController {
         return carroRepository.findAll();
     }
         
-    @RequestMapping(value="/carro", method=RequestMethod.POST)
+    @RequestMapping(value="/carro", method=RequestMethod.POST, consumes=MediaType.ALL_VALUE)
         public Carro post(@Valid @RequestBody Carro carro){
         return carroRepository.save(carro);
     }
